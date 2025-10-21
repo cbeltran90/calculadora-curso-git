39ccac74c690a2bf0648c2eb1d547ffeddbb5de5
eugeluque06 <euge.luque06@gmail.com>
2025-10-21 19:40:55 -0300

    agregue resto

---
+++ src/src/calculadora.js
@@ -0,0 +1,52 @@
+class Calculadora {
+  sumar(a, b) {
+    return a + b;
+  }
+
+  restar(a, b) {
+    return a - b;
+  }
+
+  multiplicar(a, b) {
+    return a * b;
+  }
+
+  dividir(a, b) {
+    return a / b; 
+  }
+
+  potencia(base, exponente) {
+    return Math.pow(base, exponente);
+  }
+
+  resto(a, b) {
+    return a % b;
+  }
+
+
+  raizCuadrada(numero) {
+    return Math.sqrt(numero);
+  }
+
+
+}
+
+// Exportar para usar en tests
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = Calculadora;
+}
+
+// Para usar en consola de Node.js
+const calc = new Calculadora();
+
+console.log('=== Calculadora Simple ===');
+console.log('Ejemplo de uso:');
+console.log('calc.sumar(5, 3):', calc.sumar(5, 3));
+console.log('\nFunciones disponibles:');
+console.log('- calc.sumar(a, b)');
+console.log('- calc.restar(a, b)');
+console.log('- calc.multiplicar(a, b)');
+console.log('- calc.dividir(a, b)');
+console.log('- calc.potencia(base, exponente)');
+console.log('- calc.resto(a, b)');
+console.log('- calc.raizCuadrada(numero)');
